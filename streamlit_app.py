21186f430f74f698911144c0db7bb4b386c7e701
Jagan Kumar Policherla <122357551+jagan-snowflake@users.noreply.github.com>
2023-01-10 16:07:34 +0530

    Update streamlit_app.py
    Added symbols

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents New Health Diner')
+streamlit.header('Breakfast Favorites')
 
-streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-               
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
